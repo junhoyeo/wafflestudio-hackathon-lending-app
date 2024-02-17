0a098f0d43d603d6c00c42289dc3b821d3c4818d
Junho Yeo <i@junho.io>
2024-02-18 04:56:18 +0900

    Setup shadcn/ui

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Button } from '@/components/ui/button';
 
 function Hello() {
   return (
     <div>
       <h1 className="bg-gray-500 text-center text-white">
-        Hi Tailwind has been integrated.
+        <Button>SSS</Button>
       </h1>
     </div>
   );
